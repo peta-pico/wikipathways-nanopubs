0e05a00c7f8b83116d18d08bb7e8cf6de08c7356
Tobias Kuhn <kuhntobias@gmail.com>
2017-12-14 14:44:09 +0100

    Add script

---
+++ scripts/build-diff.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Usage:
+#
+# $ scripts/build-diff.sh complexes 20161210 20170210
+#
+
+npop reuse -n -s \
+  -x output/$1/$2_full.trig.gz \
+  -o output/$1/$3-$2_new.trig.gz \
+  -a output/$1/$3_full.trig.gz \
+  -c output/cache/$1/$3_cache.txt.gz \
+  -r stats/$1_ctable.csv \
+  -t '-h WikipathwaysTopics' \
+  -f '-h WikipathwaysFingerprints' \
+  input/$1/$3.trig.gz \
+  &> reports/$1/$3-$2_report.txt
+
+npop ireuse -s \
+  -x output/$1/$2_index.trig.gz \
+  -o output/$1/$3-$2_index.trig.gz \
+  -a output/$1/$3_index.trig.gz \
+  -r stats/$1_itable.csv \
+  -T "Nanopubs covering $1 extracted from WikiPathways version $3" \
+  -C "0000-0001-7542-0286" \
+  -C "0000-0002-1267-0234" \
+  output/cache/$1/$3_cache.txt.gz \
+  &>> reports/$1/$3-$2_report.txt
+
