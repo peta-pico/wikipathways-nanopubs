750bc5cc4209aa595d6bd2b953751e5fc8f9ea66
Tobias Kuhn <kuhntobias@gmail.com>
2017-12-15 15:50:14 +0100

    Improve counting script

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Nanopublications for WikiPathways
 This repository contains the data and code for the incremental releases of the
 nanopublications dataset for WikiPathways.
 
+This code requires [npop](https://github.com/tkuhn/npop) and `npop/bin` needs to
+be in the PATH variable for the scripts to work.
+
 To create a new release, the compressed file with the raw data (with a name like
 `wp_nanopubs_20161210.tar.gz` and containing files like
 `20161210/complexes.wp1.trig`) should be placed in the directory `input/raw`.
---
+++ scripts/make-stats.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Usage:
+# $ scripts/make-stats.sh complexes HEADER
+# $ scripts/make-stats.sh complexes 20161210
+#
+
+
+# count content nanopubs:
+
+if [ $2 == "HEADER" ]; then
+  echo "filename,nanopubs,head,assertion,provenance,pubinfo" \
+    > stats/$1_count.csv
+  exit
+fi
+
+if [ -f output/$1/$2-*_new.trig.gz ]; then
+  npop count \
+    -r stats/$1_count.csv \
+    output/$1/$2-*_new.trig.gz
+fi
+
+if [ -f output/$1/$2_full.trig.gz ]; then
+  npop count \
+    -r stats/$1_count.csv \
+    output/$1/$2_full.trig.gz
+fi
+
+
+# count index nanopubs:
+
+if [ $2 == "HEADER" ]; then
+  echo "filename,nanopubs,head,assertion,provenance,pubinfo" \
+    > stats/$1_icount.csv
+  exit
+fi
+
+if [ -f output/$1/$2_index.trig.gz ]; then
+  npop count \
+    -r stats/$1_icount.csv \
+    output/$1/$2_index.trig.gz
+fi
+if [ -f output/$1/$2-*_index.trig.gz ]; then
+  npop count \
+    -r stats/$1_icount.csv \
+    output/$1/$2-*_index.trig.gz
+fi
